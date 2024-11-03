395f7da7cd0c6b64e6824c8e68b2f1d2ef08456f
Yuri Moura <mourayuri93@gmail.com>
2024-11-03 11:25:41 -0300

    atualizando projeto

---
+++ src/main/java/org/example/data/DataConfiguration.java
@@ -0,0 +1,37 @@
+package org.example.data;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataConfiguration {
+
+    // Configuração do DataSource para conexão com o banco de dados
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3307/financas?useTimezone=true&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    // Configuração do JpaVendorAdapter para o provedor JPA (Hibernate)
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
+}
